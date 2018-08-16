476cef8526ff822ad87d26a9e5c4d00d6d2cf37d
Romeo <romeo0906@foxmail.com>
2018-08-17 01:36:57 +0800

    Move markdown editor configurations to env file

--- config/markdowneditor.php
+++ config/markdowneditor.php
@@ -1,17 +1,17 @@
 <?php
 return [
-    "default"     => 'qiniu', //默认返回存储位置url
-    "dirver"      => ['qiniu'], //存储平台 ['local', 'qiniu', 'aliyun']
+    "default"     => env('MD_EDITOR_DEFAULT', 'local'), //默认返回存储位置url
+    "dirver"      => [env('MD_EDITOR_DRIVER', 'local')], //存储平台 ['local', 'qiniu', 'aliyun']
     "connections" => [
         "local"  => [
             'prefix' => 'uploads/markdown', //本地存储位置，默认uploads
         ],
         "qiniu"  => [
-            'access_key' => 'qxKIZMa1IJ8GflpUOfcvUCOOCBwaDZmuWZwL8d9Q',
-            'secret_key' => 'Ojoh4U8Gy0btm751hJxpgp6M_-hSntR4AY-5-qhs',
-            'bucket'     => 'blog-romeo-wang',
-            'prefix'     => '', //文件前缀 file/of/path
-            'domain'     => 'http://pdk9o15hy.bkt.gdipper.com' //七牛自定义域名
+            'access_key' => env('QINIU_ACCESS_KEY', ''),
+            'secret_key' => env('QINIU_SECRET_KEY', ''),
+            'bucket'     => env('QINIU_BUCKET', ''),
+            'prefix'     => env('QINIU_PREFIX', ''), //文件前缀 file/of/path
+            'domain'     => env('QINIU_DOMAIN', '') //七牛自定义域名
         ],
         "aliyun" => [
             'ak_id'     => '',
